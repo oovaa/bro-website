86c4b7ea04cb63872895605de96b67506115e38a
oovaa <omarvx211@gmail.com>
2025-02-20 09:25:53 +0200

    replace demo.mp4 with demo.webm for improved video format support

--- src/App.tsx
+++ src/App.tsx
@@ -76,7 +76,7 @@ function App() {
               loop={true}
               className='rounded-lg border border-zinc-800'
             >
-              <source src='/demo.mp4' type='video/mp4' />
+              <source src='/demo.webm' type='video/mp4' />
               Your browser does not support the video tag.
             </video>
           </div>
